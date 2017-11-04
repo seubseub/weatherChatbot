9931f274c571333d023bcc54299d19a8c3f68360
2017030091 <2017030091@hanyang.ac.kr>
2017-11-05 03:58:54 +0900

    fix

--- app.py
+++ app.py
@@ -1,75 +1,84 @@
 #coding:utf-8
-
+from pyowm import OWM
 from flask import Flask
 from flask import jsonify, request
 
-from pyowm import OWM
-
 app = Flask(__name__)
+
+API_KEY = 'e4e96bc8ffc8af544c30f6c787eaa804'
+owm = OWM(API_KEY)
+obs = owm.weather_at_place('Seoul')
+w = obs.get_weather()
+
+result = str(w.get_temperature(unit='celsius')['temp'])
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+
 @app.route('/keyboard')
 def Keyboard():
     dataSend = {
-        "type" : "buttons",
-        "buttons" : ["현재 날씨를 보여줘", "예보에 비해 더웠다", "예보가 적절했다", "예보에 비해 추웠다"]
+        "type": "buttons",
+        "buttons": ["현재 날씨를 보여줘", "예보에 비해 더웠다", "예보가 적절했다", "예보에 비해 추웠다"]
     }
     return jsonify(dataSend)
 
+
 @app.route('/message', methods=['POST'])
 def Message():
-    dataReceive = request.get_json()
-    content = dataReceive['content']
-    if content == u"현재 날씨를 보여줘":
-        API_KEY = 'e4e96bc8ffc8af544c30f6c787eaa804'
-        owm = OWM(API_KEY)
-        obs = owm.weather_at_place('Seoul')
-        w = obs.get_weather()
-        dataSend = {
-            "message": {
-                "text": "현재 온도는 : " + str(w.get_temperature(unit='celsius')['temp']) + "도 입니다"
-            },
-            "keyboard": {
-                "type": "buttons",
-                "buttons" : ["현재 날씨를 보여줘", "예보에 비해 더웠다", "예보가 적절했다", "예보에 비해 추웠다"]
+    try:
+        print(request.get_json())
+        dataReceive = request.get_json()
+        print(dataReceive)
+        content = dataReceive['content']
+        if content == u"현재 날씨를 보여줘":
+            dataSend = {
+                "message": {
+                    "text": "현재 온도는 : " + result + "도 입니다"
+                },
+                "keyboard": {
+                    "type": "buttons",
+                    "buttons": ["현재 날씨를 보여줘", "예보에 비해 더웠다", "예보가 적절했다", "예보에 비해 추웠다"]
+                }
             }
-        }
-    elif content == u"예보에 비해 더웠다":
-        dataSend = {
-            "message": {
-                "text": "귀하의 의견 감사합니다.1"
-            },
-            "keyboard": {
-            "type": "buttons",
-            "buttons" : ["현재 날씨를 보여줘", "예보에 비해 더웠다", "예보가 적절했다", "예보에 비해 추웠다"]
+        elif content == u"예보에 비해 더웠다":
+            dataSend = {
+                "message": {
+                    "text": "귀하의 의견 감사합니다.1"
+                },
+                "keyboard": {
+                    "type": "buttons",
+                    "buttons": ["현재 날씨를 보여줘", "예보에 비해 더웠다", "예보가 적절했다", "예보에 비해 추웠다"]
+                }
             }
-        }
 
-    elif content == u"예보가 적절했다":
-        dataSend = {
-            "message": {
-                "text": "귀하의 의견 감사합니다.2"
-            },
-            "keyboard": {
-             "type": "buttons",
-             "buttons" : ["현재 날씨를 보여줘", "예보에 비해 더웠다", "예보가 적절했다", "예보에 비해 추웠다"]
+        elif content == u"예보가 적절했다":
+            dataSend = {
+                "message": {
+                    "text": "귀하의 의견 감사합니다.2"
+                },
+                "keyboard": {
+                    "type": "buttons",
+                    "buttons": ["현재 날씨를 보여줘", "예보에 비해 더웠다", "예보가 적절했다", "예보에 비해 추웠다"]
+                }
             }
-        }
-    elif content == u"예보에 비해 추웠다":
-        dataSend = {
-            "message": {
-                "text": "귀하의 의견 감사합니다.3"
-            },
-            "keyboard": {
-            "type": "buttons",
-            "buttons" : ["현재 날씨를 보여줘", "예보에 비해 더웠다", "예보가 적절했다", "예보에 비해 추웠다"]
+        elif content == u"예보에 비해 추웠다":
+            dataSend = {
+                "message": {
+                    "text": "귀하의 의견 감사합니다.3"
+                },
+                "keyboard": {
+                    "type": "buttons",
+                    "buttons": ["현재 날씨를 보여줘", "예보에 비해 더웠다", "예보가 적절했다", "예보에 비해 추웠다"]
+                }
             }
-        }
-    return jsonify(dataSend)
+        return jsonify(dataSend)
+    except Exception as e:
+        print(e)
+        return ""
 
 if __name__ == '__main__':
     app.run()
